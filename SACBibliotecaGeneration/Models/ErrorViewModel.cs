c3056d9637138f40733f0211dbc7aff90d9fd380
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-06-01 14:27:49 +0200

    Aggiungere i file di progetto.

---
+++ SACBibliotecaGeneration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SACBibliotecaGeneration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
